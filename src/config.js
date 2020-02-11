ad740afcb43faa29d5edeb1683d5bdfeb5c42842
Karina Gaulin <karina@domandtom.com>
2020-02-11 12:04:35 -0500

    Add back

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   PORT: process.env.PORT || 8000,
-  API_BASE_URL: process.env.REACT_APP_BASE_URL
+  API_BASE_URL: process.env.REACT_APP_BASE_URL || "http://localhost:8000/api",
 }

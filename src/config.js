c86b3a982f764ac5bb6f03c5be350482f909210b
Karina Gaulin <karina@domandtom.com>
2020-02-11 14:20:18 -0500

    Add api to end of url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   PORT: process.env.PORT || 8000,
-  API_BASE_URL: process.env.NODE_ENV === 'production' ? 'https://fierce-coast-06372.herokuapp.com/' : "http://localhost:8000/api",
+  API_BASE_URL: process.env.NODE_ENV === 'production' ? 'https://fierce-coast-06372.herokuapp.com/api' : "http://localhost:8000/api",
 }
